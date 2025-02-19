264617b868a12a8183c01e9e143ca38e2871f481
ellie-web <ellie.k.1711@gmail.com>
2025-02-20 00:14:15 +0300

    add user page following status

--- components/users/Details.vue
+++ components/users/Details.vue
@@ -19,7 +19,7 @@
         </div>
 
       </div>
-      <div class="flex items-center justify-center gap-5">
+      <div class="flex items-center justify-center gap-5 mb-4">
         <div class="flex flex-col items-center">
           <p>{{ userContext?.followers }}</p>
           <p>followers</p>
@@ -29,9 +29,26 @@
           <p>following</p>
         </div>
       </div>
+      <div class="text-gray-500">{{ followStatus }}</div>
     </template>
   </UCard>
 </template>
 <script setup lang="ts">
+import useSWRV from 'swrv';
+
 const { userContext } = useUserContext()
+
+const fetchCheck = () => $fetch(`/api/users/${userContext?.value.id}/follow/check`)
+
+const { data: subscriptionStatus } = useSWRV(`follow/check/${userContext?.value.id}`, fetchCheck)
+
+const followStatus = computed(() => {
+  if (!subscriptionStatus.value) return undefined
+  if (subscriptionStatus.value.isFollowing && subscriptionStatus.value.isBeingFollowed) {
+    return 'You follow each other'
+  }
+  if (subscriptionStatus.value.isBeingFollowed) {
+    return 'Follows you'
+  }
+})
 </script>
