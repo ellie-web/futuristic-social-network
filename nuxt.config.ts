fd16861b213a98cc35fef31fd503bc552ac8c90e
ellie-web <ellie.k.1711@gmail.com>
2025-02-23 01:58:31 +0300

    add naive ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,7 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -7,12 +11,32 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/image',
     'nuxt-file-storage',
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    'nuxtjs-naive-ui'
   ],
   fileStorage: {
     mount: process.env.FILES_DIR
   },
   runtimeConfig: {
     filesDir: process.env.FILES_DIR
+  },
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
   }
 })
